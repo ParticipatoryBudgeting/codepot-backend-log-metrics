fd6882118454646b34ef510f3c055355254c3299
Marcin Jasion <marcin.jasion@gmail.com>
2015-08-19 23:54:31 +0200

    fix

--- codepot-backend-log-metrics.sh
+++ codepot-backend-log-metrics.sh
@@ -2,18 +2,18 @@
 
 error_counter() {
     errorCode=$1
-    errors_total=`grep " ${errorCode} " /var/log/nginx/backend.codepot.access.log`
-    errors_today=`echo $errors_total | grep \`date +"%d/%b"\``
-    errors_ips=`echo $errors_today | cut -d ' ' -f 1 | sort -u`
+    errors_total=$(grep " ${errorCode} " /var/log/nginx/backend.codepot.access.log)
+    errors_today=$(echo "$errors_total" | grep `date +"%d/%b"`)
+    errors_ips=$(echo "$errors_today" | cut -d ' ' -f 1 | sort -u)
 
-    errors_counter=`echo $errors_today | wc -l`
-    errors_ip_counter=`echo $errors_ips | wc -l`
+    errors_counter=$(echo "$errors_today" | wc -l)
+    errors_ip_counter=$(echo "$errors_ips" | wc -l)
 
     echo "errors-${errorCode}-total-count value=$errors_counter"
     echo "errors-${errorCode}-ip-count value=$errors_ip_counter"
 
     for ip in $errors_ips; do
-            by_ip_counter=`echo $errors_total | grep $ip | wc -l`
+            by_ip_counter=$(echo "$errors_total" | grep $ip | wc -l)
             echo "error-${errorCode}-by-ip-counter-$ip value=$by_ip_counter"
     done
 }
